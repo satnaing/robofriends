1f0596d6878c37dbaed5caa50ada7f5f8c03098f
Sat Naing <satnaingdev@gmail.com>
2020-10-21 21:59:29 +0630

    Updates

---
+++ precache-manifest.f6213b2f7cb58f015884a0346f9ac99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e62147083c0a583a2881b9176dad5ce",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0ac063ed4c470d4ed072",
+    "url": "/robofriends/static/css/main.decc7355.chunk.css"
+  },
+  {
+    "revision": "45ef0a63bf9be9038ae9",
+    "url": "/robofriends/static/js/2.016ce72c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends/static/js/2.016ce72c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac063ed4c470d4ed072",
+    "url": "/robofriends/static/js/main.3461f0ce.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
